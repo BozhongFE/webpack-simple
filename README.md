d32b357553801326eac37206483c97a8929ec7c6
xclazy <sue561833@qq.com>
2018-06-27 16:28:25 +0800

    内置HtmlAutoDomainWebpackPlugin插件优化
    原适用于单个html，现需传入配置适用任意个html

--- README.md
+++ README.md
@@ -22,10 +22,11 @@ $ npm run dev
 
 ### HISTORY
 
-|#|日期|开发内容|
-|---|---|---|
-|#|20180510|fork from [vuejs-templates/webpack-simple](https://github.com/vuejs-templates/webpack-simple)
-|#|20180510|依赖调整、sass更换成less、新增reset.css、打包输出到source;
-|#|20180530|新增html-webpack-plugin;
-|#|20180612|新增重定向后basehref/脚本引用相关;
-|#|20180612|新增webpack-bundle-analyzer;
+|#|标签|日期|开发内容|
+|---|---|---|---|
+|#|v0.1.0|20180510|fork from [vuejs-templates/webpack-simple](https://github.com/vuejs-templates/webpack-simple)
+|#|v0.1.0|20180510|依赖调整、sass更换成less、新增reset.css、打包输出到source;
+|#|v0.1.0|20180530|新增html-webpack-plugin;
+|#|v0.1.0|20180612|新增重定向后basehref/脚本引用相关;
+|#|v0.1.0|20180615|新增webpack-bundle-analyzer;
+|#|v0.2.0|20180627|内置HtmlAutoDomainWebpackPlugin插件优化
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -20,61 +20,84 @@ if (typeof sourcePath === 'undefined') {
 {{/source}}
 {{#redirected}}
 // 给插入html的js加版本号
-function HtmlAutoDomainWebpackPlugin(options) {
-}
-
-HtmlAutoDomainWebpackPlugin.prototype.apply = (compiler) => {
-  const sortMap = {
-    main: 0,
-  };
-  compiler.plugin('compile', (params) => {});
-  compiler.plugin('compilation',  (compilation) => {
-    compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
-      const source = fs.readFileSync(path.resolve(__dirname, './src/index.html'), 'utf-8');
-      const files = [];
-      chunks.forEach((chunk) => {
-        if (chunk.name) {
-          chunk.files.forEach((file) => {
-            // 只筛选所有页面公用部分
-            if (sortMap.hasOwnProperty(file.match(/(.+).js/)[1])) {
-              files.push(file);
+class HtmlAutoDomainWebpackPlugin {
+  constructor (options) {
+    const obj = {};
+    for (let i in options) {
+      if (Object.prototype.toString.call(options[i]) === '[object Object]') {
+        const key = options[i].filename;
+        if (key) {
+          obj[key] = {};
+          if (options[i].chunks) {
+            const chunks = options[i].chunks;
+            for (let j in chunks) {
+              obj[key][chunks[j]] = j;
             }
-          });
+          }
         }
-      });
-      files.sort((a, b) => {
-        let aName = a.match(/(.+).js/);
-        let bName = b.match(/(.+).js/);
-        if (aName.length >= 1) {
-          aName = aName[1];
-        }
-        if (bName.length >= 1) {
-          bName = bName[1];
-        }
-        // console.log(aName, bName);
-        return sortMap[aName] > sortMap[bName];
-      });
-      console.log(files);
-      compilation.assets['./index.html'] = {
-        source() {
-          const dom = new jsdom.JSDOM(source);
-          const script = dom.window.document.createElement('script');
-          console.log(files);
-          const hash = files[0].match(/\?(.+)/);
-          let html = `var hash = '${hash && hash[1] ? hash[1] : ''}';`;
-          html += `var files = ${JSON.stringify(files)};`;
-          script.innerHTML = html;
-          dom.window.document.querySelector('head').appendChild(script)
-          return dom.serialize();
-        },
-        size() {
-          return source.length;
-        },
-      };
-      callback();
+      }
+    }
+    this.options = obj;
+  }
+  apply(compiler) {
+    const configs = this.options;
+  
+    compiler.plugin('compile', (params) => {
     });
-  })
-};
+    compiler.plugin('compilation',  (compilation) => {
+      compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+        const files = {};
+        chunks.forEach((chunk) => {
+          if (chunk.name) {
+            chunk.files.forEach((file) => {
+              // 只筛选所有页面公用部分
+              for (let key in configs) {
+                console.log(configs, key)
+                if (configs[key].hasOwnProperty(file.match(/(.+).js/)[1])) {
+                  if (!files[key]) files[key] = [];
+                  files[key].push(file);
+                }
+              }
+            });
+          }
+        });
+        console.log(files)
+        for (let key in files) {
+          const source = fs.readFileSync(path.resolve(__dirname, `./src/${key}`), 'utf-8');
+          const config = configs[key];
+          files[key].sort((a, b) => {
+            let aName = a.match(/(.+).js/);
+            let bName = b.match(/(.+).js/);
+            if (aName.length >= 1) {
+              aName = aName[1];
+            }
+            if (bName.length >= 1) {
+              bName = bName[1];
+            }
+            return config[aName] > config[bName];
+          });
+          compilation.assets[`./${key}`] = {
+            source() {
+              const dom = new jsdom.JSDOM(source);
+              const script = dom.window.document.createElement('script');
+              // console.log(files);
+              const hash = files[key][0].match(/\?(.+)/);
+              let html = `var hash = '${hash && hash[1] ? hash[1] : ''}';`;
+              html += `var files = ${JSON.stringify(files[key])};`;
+              script.innerHTML += html;
+              dom.window.document.querySelector('head').appendChild(script)
+              return dom.serialize();
+            },
+            size() {
+              return source.length;
+            },
+          };
+        };
+        callback();
+      })
+    })
+  }
+}
 {{/redirected}}
 
 
@@ -194,7 +217,13 @@ if (process.env.NODE_ENV === 'production') {
     }),
     new BundleAnalyzerPlugin(),
     {{#redirected}}
-    new HtmlAutoDomainWebpackPlugin(),
+    // 给html注入的js加版本号，用于重定向时文件引入
+    new HtmlAutoDomainWebpackPlugin([
+      {
+        filename: 'index.html',
+        chunks: ['main'],
+      },
+    ]),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/index.html',
